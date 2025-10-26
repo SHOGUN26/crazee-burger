88f73def7afba268ee8f8481c5502c2d2c4b5bde
Assemien <xavierassemien610@gmail.com>
2025-10-26 15:04:19 +0000

    ✨  F01: LOGIN>  add title and form + font

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  // state
+  const [prenom, setprenom] = useState();
+  // comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${prenom}`);
+    setprenom("");
+  };
+
+  // affichage (render)
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setprenom(e.target.value)}
+          type="text"
+          placeholder="Entrez votre prénom"
+          required
+          value={prenom}
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 }
