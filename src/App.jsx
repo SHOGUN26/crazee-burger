2f6a6271ed272b9b529dfe529e95d2857a26a6bd
Assemien <xavierassemien610@gmail.com>
2025-10-26 11:32:02 +0000

    validation du 1er ticket

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [prenom, setprenom] = useState("");
+
+  const handleClick = () => {
+    // e.preventDefault();
+    if (prenom.trim() !== "") {
+      alert(`Bienvenu ${prenom}`);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>bienvenue chez nous !</h1>
+      <h2>Connectez vous</h2>
+      <form>
+        <input
+          value={prenom}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+          onChange={(e) => setprenom(e.target.value)}
+        />
+        <button onClick={handleClick}>Accéder à votre espace</button>{" "}
+      </form>
     </>
   );
 }
