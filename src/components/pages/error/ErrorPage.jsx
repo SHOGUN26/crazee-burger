96552f12d19cba71f6a1762c19772ef06be6f3b6
Assemien <xavierassemien610@gmail.com>
2025-10-27 12:18:45 +0000

    ✨ F02:LOGIN> add redirection form/to ErrorPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div>
+      <h1>Error</h1>
+      <Link to="/">
+        <button>Retourner vers la page d'acceuil</button>
+      </Link>
+    </div>
+  );
 }
